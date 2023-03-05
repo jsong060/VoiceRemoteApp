2166966f21bb00caeeee19cdb267644fce8ee134
jsong060 <jsong060@uottawa.ca>
2023-03-04 22:48:45 -0500

    implemented the Speech-To-Text algorithm

--- app/src/main/java/com/example/voiceremoteapp/MainActivity.java
+++ app/src/main/java/com/example/voiceremoteapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.voiceremoteapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -16,6 +17,8 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,12 +28,118 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn;
+    private TextView tempText;
+    private SpeechRecognizer speechRecognizer;
+    private boolean btnClicked = false;
+
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //grant permission to mic
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            checkPermission();
+        }
+
+        btn = (Button) findViewById(R.id.button);
+        tempText = (EditText) findViewById(R.id.textbox);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+                tempText.setText("Ready for transcription!");
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                tempText.setText("????");
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                tempText.setText("!!!!!");
+                ArrayList<String> txt = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                tempText.setText(txt.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnClicked == false){
+                    speechRecognizer.startListening(speechIntent);
+                }else{
+                    speechRecognizer.stopListening();
+                }
+            }
+        });
     }
 
+    private void checkPermission() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO_PERMISSION);
+        }
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            if (grantResults.length > 0 && grantResults != null) {
+                // Permission has been granted
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                // Permission denied, handle the case accordingly
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
